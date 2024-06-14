b7cb6ebf8307821d054e9d3b7daf3fea4b52ab1c
s-damasceno <samueldamasceno.vip@gmail.com>
2024-06-14 17:41:26 -0300

    lógica para cartas especiais implementada

--- cartas.py
+++ cartas.py
@@ -6,18 +6,29 @@ class CartaComum():
         self.cor = cor
 
 class CartaEspecial():
-    def __init__(self, nome, efeito):
+    def __init__(self, nome, cor=None):
         self.nome = nome
-        self.efeito = efeito
+        self.cor = cor
 
 
 class Baralho():
     def __init__(self):
+        self.cartas = []
+
         numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         cores = ["Vermelho", "Azul", "Verde", "Amarelo"]
-        cartasEspeciais = ["+2", "+4", "Coringa", "Inverter", "Bloqueio"]
-        self.cartas = []
+        cartasEspeciais = ["+2", "Inverter", "Bloqueio"]
+        coringas = ["+4", "Coringa"]
+
         for cor in cores:
             for numero in numeros:
                 carta = CartaComum(numero, cor)
+                self.cartas.append(carta)
+            for cartaEspecial in cartasEspeciais:
+                for i in range(2):
+                    carta = CartaEspecial(cartaEspecial, cor)
+                    self.cartas.append(carta)
+        for coringa in coringas:
+            for i in range(4):
+                carta = CartaEspecial(coringa)
                 self.cartas.append(carta)
